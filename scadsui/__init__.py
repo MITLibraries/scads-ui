8f93bba2ef9ffa5f23c2ae1aa8ac615669bb1ace
Helen Bailey <hakbailey@gmail.com>
2015-11-06 15:58:19 -0500

    Update config for heroku

--- scadsui/__init__.py
+++ scadsui/__init__.py
@@ -1,10 +1,15 @@
 from __future__ import absolute_import
 
 from flask import Flask
-import os
+from config import Config
+
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(Config)
 
 import scadsui.forms
 import scadsui.views
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
