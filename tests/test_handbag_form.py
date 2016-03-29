7087bfadb34572de3641d08261ee4c90fc2f9acb
Helen Bailey <hakbailey@gmail.com>
2016-03-29 14:58:00 -0400

    Update form defaults and submit all workflows to anon agent. Closes #6, closes #7, closes #8, closes #9.

--- tests/test_handbag_form.py
+++ tests/test_handbag_form.py
@@ -11,21 +11,24 @@ def test_form_loads(testapp):
     assert 'form' in response
 
 
-def test_add_metadata_field(testapp):
+def test_add_and_delete_metadata_field(testapp):
     handbag_page = testapp.get('/handbag')
     form = handbag_page.form
-    assert 'metadata-item' not in handbag_page
+    assert 'metadata-item' in handbag_page
+    delete = form.submit('delMetadata')
+    assert 'metadata-item' not in delete
     add = form.submit('addMetadata')
     assert 'metadata-item' in add
 
 
-def test_submit_valid_form_without_metadata(app, testapp):
+def test_submit_valid_form(app, testapp):
     form = testapp.get('/handbag').form
     form['name'] = 'test'
     form['destinationName'] = 'test'
-    form['destinationUrl'] = 'test'
-    form['destinationEmail'] = 'test'
+    form['destinationUrl'] = 'file:///safadf'
+    form['destinationEmail'] = 'test@test.com'
     form['bagNameGenerator'] = 'test'
     form['packageFormat'] = 'zip'
     results = form.submit()
+    print(results)
     assert results.status_code == 302
